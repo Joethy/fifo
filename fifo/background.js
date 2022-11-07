bc35a00711123da25383bb13a4196c9bc17e8fec
Joethy <61763778+Joethy@users.noreply.github.com>
2022-11-08 07:37:52 +1300

    Added new method for moving bookmarks/folder

--- fifo/background.js
+++ fifo/background.js
@@ -1,7 +1,31 @@
+let trashId;
+
+chrome.bookmarks.getRootByName('trash', function(object) {
+	trashId = object.id;
+});
+
 chrome.bookmarks.onCreated.addListener(function(id, bookmark) {
-	
+	moveToEnd(bookmark,"create");
 });
 
 chrome.bookmarks.onMoved.addListener(function(id, moveInfo) {
-	
+	if (moveInfo.parentId == trashId) { return; }
+
+	if (moveInfo.parentId != moveInfo.oldParentId) {
+		if (moveInfo.index == 0) {
+			moveToEnd({"id":id, "parentId":moveInfo.parentId}, "move");
+		}
+	}
 });
+
+function moveToEnd(node, source) {
+	chrome.bookmarks.get(node.parentId, function(parent) {
+		parentId = parent[0].id;
+
+		chrome.bookmarks.getChildren(parentId, function(children) {
+			endIndex = children.length-1;
+			
+			console.log(node.id+" "+source);
+		});
+	});
+}
