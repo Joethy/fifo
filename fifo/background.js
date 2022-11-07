d0f50b92e5ba9c38485d72d01027c08f93322349
Joethy <61763778+Joethy@users.noreply.github.com>
2022-11-08 07:45:47 +1300

    finalized everything

--- fifo/background.js
+++ fifo/background.js
@@ -1,31 +1,32 @@
-let trashId;
+let trashId; // ID of the bookmarks trash bin
 
+// retrieve and store ID of bookmarks trash bin
 chrome.bookmarks.getRootByName('trash', function(object) {
-	trashId = object.id;
+	trashId = object.id; 
 });
 
+// add listener for bookmark creation
 chrome.bookmarks.onCreated.addListener(function(id, bookmark) {
-	moveToEnd(bookmark,"create");
+	moveToEnd(bookmark); // Move bookmark to end
 });
 
+// add listener for bookmark move
 chrome.bookmarks.onMoved.addListener(function(id, moveInfo) {
-	if (moveInfo.parentId == trashId) { return; }
+	if (moveInfo.parentId == trashId) { return; } // if move to trash dont do anything
 
+	// If moved to new folder
 	if (moveInfo.parentId != moveInfo.oldParentId) {
-		if (moveInfo.index == 0) {
-			moveToEnd({"id":id, "parentId":moveInfo.parentId}, "move");
+		if (moveInfo.index == 0) { // if left to go to first index
+			moveToEnd({"id":id, "parentId":moveInfo.parentId}); // move to bottom of list
 		}
 	}
 });
 
-function moveToEnd(node, source) {
-	chrome.bookmarks.get(node.parentId, function(parent) {
-		parentId = parent[0].id;
+// function to move node (bookmark or folder) to bottom of list
+function moveToEnd(node) {
+	chrome.bookmarks.getChildren(node.parentId, function(children) { // get all children of parent node
+		endIndex = children.length; // Get length (also end index)
 
-		chrome.bookmarks.getChildren(parentId, function(children) {
-			endIndex = children.length-1;
-			
-			console.log(node.id+" "+source);
-		});
+		chrome.bookmarks.move(node.id, {"index":endIndex}); // move node
 	});
 }
